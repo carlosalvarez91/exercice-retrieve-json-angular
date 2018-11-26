4a0d199ceff50c803ecb44e06703e6570d3b5150
carlos <calvarez@seeingnewdata.com>
2018-11-26 18:00:49 +0000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Image } from './image.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'runpath';
+
+  
+  images: Image[];
+
+
+  constructor(
+
+    private appService:AppService
+
+    ){}
+
+    ngOnInit(){
+      this.getImages()
+    }
+
+
+    getImages(){
+      this.appService.getImages()
+      .subscribe(images => {
+
+        console.log(images)
+
+        this.images = images
+      });
+    }
+
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Image } from './image.model';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+
+  images: Image[] = [];
+
+  private url:string = 'http://jsonplaceholder.typicode.com/photos';
+  
+  constructor(private http: HttpClient) { }
+
+
+  getImages(): Observable<Image[]> {
+    return this.http.get<Image[]>(this.url)
+  }
+}
