e64cfc18836450cdff8c134e33f5424022c70b37
carlos <calvarez@seeingnewdata.com>
2018-11-27 12:19:26 +0000

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Image } from './image.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     AppRoutingModule,
     HttpClientModule,
     NgxPaginationModule
+
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
